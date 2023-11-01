597edd25a7bdbb738d352b809586bead22b188cc
anissalopez <anissa965@gmail.com>
2023-11-01 13:30:41 -0400

    working with one to many object relationships

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,41 +1,45 @@
 # owner_pet.py
 
-class Pet:
-    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
-    all = []
-
-    def __init__(self, name, pet_type, owner=None):
-        self.name = name
-        if pet_type in self.PET_TYPES:
-            self.pet_type = pet_type
-        else:
-            raise ValueError("Invalid pet type")
-        self._owner = None
-        if owner is not None:
-            self.owner = owner
-        Pet.all.append(self)
-
-    def owner(self):
-        return self._owner
-
-    def owner(self, owner):
-        if not isinstance(owner, Owner):
-            raise ValueError("Owner must be an instance of Owner class")
-        self._owner = owner
-
 class Owner:
     def __init__(self, name):
         self.name = name
 
     def pets(self):
-        return [pet for pet in Pet.all if pet.owner == self]
-
+        return [pet if pet.owner == self else None for pet in Pet.all]
+    
     def add_pet(self, pet):
-        if not isinstance(pet, Pet):
-            raise ValueError("Pet must be an instance of Pet class")
-        pet.owner = self
-
+        if isinstance(pet, Pet):
+            pet.owner = self
+        else:
+            raise Exception
+        
     def get_sorted_pets(self):
-        pets = [pet for pet in Pet.all if pet.owner == self]
-        return sorted(pets, key=lambda pet: pet.name)
+        pet_list = [pet for pet in Pet.all if pet.owner == self]
+        pet_list.sort(key = lambda x: x.name)
+        return pet_list
 
+
+
+
+class Pet:
+    all = []
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"] 
+    def __init__(self, name, pet_type, owner = None):
+        self.name = name 
+        self.pet_type = pet_type 
+        self.owner = owner
+        Pet.all.append(self)
+    
+    @property
+    def pet_type(self):
+        return self._pet_type
+    
+    @pet_type.setter
+    def pet_type(self, value):
+        if value not in self.PET_TYPES:
+            raise Exception
+        self._pet_type = value
+
+
+
+ 
